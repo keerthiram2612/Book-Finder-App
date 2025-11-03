2880b59883d5649361d47d107f09d2de6de2b4f6
Keerthana <keerthiram2612@gmail.com>
2025-11-03 12:17:42 +0530

    push

--- src/utils/api.js
+++
@@ -1,7 +0,0 @@
-export const searchBooks = async (title) => {
-  const url = `https://openlibrary.org/search.json?title=${encodeURIComponent(title)}&limit=20`;
-  const res = await fetch(url);
-  if (!res.ok) throw new Error('Network response was not ok');
-  const data = await res.json();
-  return data; // contains docs array
-};
